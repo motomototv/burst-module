eb3195b64ddd7eafe7ec0ad96adee122193077b2
godcong <jumbycc@163.com>
2021-12-14 16:24:47 +0800

    feat(plugin): add burst plugin

---
+++ api.go
@@ -0,0 +1,36 @@
+package plugin
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type GinFunc struct {
+	Path   string
+	Method string
+	Func   gin.HandlerFunc
+}
+
+type HTTPFunc struct {
+	Path   string
+	Method string
+	Func   func(http.ResponseWriter, *http.Request)
+}
+
+// HandleHTTP ...
+// @Description:
+type HandleHTTP interface {
+	ListHTTP() []*HTTPFunc
+}
+
+// HandleGin ...
+// @Description:
+type HandleGin interface {
+	ListGin() []*GinFunc
+}
+
+type API interface {
+	HandleHTTP
+	HandleGin
+}
