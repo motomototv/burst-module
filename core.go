147e09601d2a0706ecb12a177bf382adee7156cd
godcong <jumbycc@163.com>
2021-12-14 15:27:00 +0800

    feat(plugin): add burst plugin

--- core.go
+++ core.go
@@ -1,8 +1,22 @@
 package plugin
 
+import (
+	"context"
+)
+
 type Plugin interface {
 	Name() string
 	Version() int
+	Start(node Node) error
 	Handler
 	Sender
+}
+
+type Node interface {
+	Context() context.Context
+	//Config() *BurstConfig
+	RepoPath() string
+	Identity() string
+	RegisterPlugin(plugin Plugin)
+	State() NodeState
 }
---
+++ state.go
@@ -0,0 +1,5 @@
+package plugin
+
+//NodeState is the status of the service
+//ENUM(invalid,starting,connecting,connectFailed,connected,syncing,syncingBoot,syncedBoot,syncBootFailed,syncingInfo,syncedInfo,syncInfoFailed,running, stopping, stopped,max)
+type NodeState int32
